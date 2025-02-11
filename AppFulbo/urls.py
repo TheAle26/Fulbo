9d6798bb594301042eaf7f7443c07623f5ca45f0
Alejo <alejo.vincent26@gmail.com>
2025-02-10 22:32:01 -0300

    Descripción de los cambios

---
+++ AppFulbo/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from AppFulbo import views
+
+urlpatterns = [
+    path('inicio/',views.inicio)
+]
--- AppFulbo/views.py
+++ AppFulbo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+# from AppFulbo.models import
+from django.http import HttpResponse, HttpResponseBadRequest
+import datetime
+# import AppFulbo.forms 
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def inicio(request):
+    return render(request,"AppFulbo/inicio.html") #como tercer argumento le tengo que pasar en forma de diccionario la info
